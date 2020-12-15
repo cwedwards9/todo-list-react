d2a20d8e9cfc5d9d3da9ba3e81430523ac6d8449
Chase Edwards <chase.w.edwards9@gmail.com>
2020-12-15 11:18:48 -0500

    Add a method, addTodo(), which takes in a new todo and adds it to the state via setState(). Bind addTodo to the constructor and pass it down as props to the NewTodoForm component.

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Todo from "./Todo";
+import NewTodoForm from "./NewTodoForm";
 
 class TodoList extends Component {
     constructor(props) {
@@ -10,15 +11,24 @@ class TodoList extends Component {
                 {todoName: "Walk the fish"},
                 {todoName: "Mow the lawn"}
             ]
-        }
+        };
+        this.addTodo = this.addTodo.bind(this);
     }
+    addTodo(newTodo) {
+        this.setState({
+            todos: [...this.state.todos, newTodo]
+        })
+    }
+
     render() {
         let todos = this.state.todos.map(todo => {
             return <Todo todoName={todo.todoName}/>
         })
         return (
             <div>
+                <h1>Todo List w/ React</h1>
                 {todos}
+                <NewTodoForm addTodo={this.addTodo}/>
             </div>
         );
     }
