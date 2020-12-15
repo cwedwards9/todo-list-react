35d36cca8f0f63b2fc7898b1148a926c7f4b3fb8
Chase Edwards <chase.w.edwards9@gmail.com>
2020-12-15 11:22:15 -0500

    Add React event to the form and input tag for handling submit and change within the form. Create a method to handle the events, handleChange to update the state as the form input value is changed, handleSubmit to call the parent method, addTodo() and pass in a new todo.

--- src/NewTodoForm.js
+++ src/NewTodoForm.js
@@ -1,11 +1,34 @@
 import React, { Component } from "react";
 
 class NewTodoForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {todoName: ""};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(evt) {
+        this.setState({
+            [evt.target.name]: evt.target.value
+        });
+    }
+    handleSubmit(evt) {
+        evt.preventDefault();
+        this.props.addTodo(this.state);
+        this.setState({todoName: ""});
+    }
+
     render() {
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <label htmlFor="newTodo">New Todo: </label>
-                <input type="text" id="newTodo" />
+                <input 
+                    type="text" 
+                    id="newTodo" 
+                    name="todoName" 
+                    value={this.state.todoName}
+                    onChange={this.handleChange}
+                />
 
                 <button>Add Todo</button>
             </form>
