3b5f82f8c23d53c6875c6be5f64e5d1c5249bfe3
Chase Edwards <chase.w.edwards9@gmail.com>
2020-12-15 11:42:09 -0500

    Add and bind handleRemove method which is called onclick of the delete (X) button. This method calls the parent component method, removeTodo which takes in a todo's id and filters it out of the todo list in the state.

--- src/Todo.js
+++ src/Todo.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 
 class Todo extends Component {
+    constructor(props) {
+        super(props);
+        this.handleRemove = this.handleRemove.bind(this);
+    }
+    handleRemove() {
+        this.props.removeTodo(this.props.id);
+    }
+
     render() {
         return (
             <div>
                 <p>{this.props.todoName}</p>
                 <button>Edit</button>
-                <button>X</button>
+                <button onClick={this.handleRemove}>X</button>
             </div>
         )
     }
