8ed0be4983d9df9104f55f8d375c7f32f0bc0634
Chase Edwards <chase.w.edwards9@gmail.com>
2020-12-15 11:43:43 -0500

    Add and bind removeTodo method which takes in an id and updates state by filtering out the todo with the same id which removes it from the list. Pass the method down to the Todo component as props.

--- src/TodoList.js
+++ src/TodoList.js
@@ -6,24 +6,33 @@ class TodoList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            todos: 
-            [
-                {todoName: "Walk the fish"},
-                {todoName: "Mow the lawn"}
-            ]
+            todos: []
         };
         this.addTodo = this.addTodo.bind(this);
+        this.removeTodo = this.removeTodo.bind(this);
     }
     addTodo(newTodo) {
         this.setState({
             todos: [...this.state.todos, newTodo]
-        })
+        });
+    }
+    removeTodo(id) {
+        this.setState({
+            todos: this.state.todos.filter(todo => todo.id !== id)
+        });
     }
 
     render() {
         let todos = this.state.todos.map(todo => {
-            return <Todo todoName={todo.todoName}/>
-        })
+            return (
+                <Todo 
+                    todoName={todo.todoName}
+                    key={todo.id}
+                    id={todo.id}
+                    removeTodo={this.removeTodo}
+                />
+        )});
+
         return (
             <div>
                 <h1>Todo List w/ React</h1>
